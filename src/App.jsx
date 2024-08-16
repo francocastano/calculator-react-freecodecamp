ba5cbc280fa62b9314c575498ffbd7c48351d7cb
francocastano <82828690+francocastano@users.noreply.github.com>
2024-08-16 23:01:06 +0000

    add initial styles for the proyect

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import freeCodeCampLogo from "./images/freecodecamp-logo.png"
+
+function App() {
+  return (
+    <div className="App">
+     <div className="container-logo-freecodecamp">
+      <img src="{freeCodeCampLogo}" alt="freeCodeCamp Logo" className="logo-freecodecamp" />
+      </div>  
+    </div>
+  );
+}
+
+export default App;
